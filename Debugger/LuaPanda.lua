c2abb634aca8951e267fd13f5df5386a700b06b0
TENCENT\stuartwang <stuartwang@tencent.com>
2019-05-22 15:53:24 +0800

    修复了连接后settimeout的错误

--- Debugger/LuaPanda.lua
+++ Debugger/LuaPanda.lua
@@ -171,7 +171,7 @@ function this.connectSuccess()
     this.changeRunState(runState.WAIT_CMD);
     this.printToConsole("connectSuccess");
     --设置初始状态
-    local ret = this.debugger_wait_msg();
+    local ret = this.debugger_wait_msg(0.05);
 
     --获取debugger文件路径
     if DebuggerFileName == "" then
