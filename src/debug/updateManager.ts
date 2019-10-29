3071f0396031a4b0dff7b9700ddd380603c8ba8b
stuartwang <3030078087@qq.com>
2019-10-29 11:47:51 +0800

    更新升级提示，加入二次确认逻辑和本次运行期间不再提示逻辑

--- src/debug/updateManager.ts
+++ src/debug/updateManager.ts
@@ -4,8 +4,18 @@ import * as vscode from 'vscode';
 import * as fs from "fs";
 
 export class UpdateManager{
+    private static checkUpdate = true;
+
+    public static setCheckUpdate(state){
+        this.checkUpdate = state;
+    }
+
     // 获取调试器lua文件的版本号，并提示用户升级
     public static checkIfLuaPandaNeedUpdate(){
+        if(!this.checkUpdate){
+            return;
+        }
+
         // 从列表中找到文件
         if(Tools.fileNameToPathMap && typeof(Tools.fileNameToPathMap["LuaPanda"]) === 'string'){
             Tools.luapandaPathInUserProj = Tools.fileNameToPathMap["LuaPanda"];
@@ -23,8 +33,19 @@ export class UpdateManager{
             if (DVerArr.length === AVerArr.length && DVerArr.length === 3 ){
                 if ( parseInt(DVerArr[0]) < parseInt(AVerArr[0]) || parseInt(DVerArr[1]) < parseInt(AVerArr[1]) || parseInt(DVerArr[2]) < parseInt(AVerArr[2])  ){
                     vscode.window.showInformationMessage('当前工程中 LuaPanda 文件版本比较低，是否自动升级为新版本?', 'Yes', 'No').then(value => {
-                        if(value === "Yes"){  this.updateLuaPandaFile(Tools.luapandaPathInUserProj) };
-                    })
+                        if(value === "Yes"){  
+                            vscode.window.showInformationMessage('已准备好更新 ' + Tools.luapandaPathInUserProj+ '  如用户对此文件有修改, 建议备份后升级。是否现在升级?', '确认升级', '稍后再试').then(value => {
+                                if(value === "确认升级"){
+                                    UpdateManager.updateLuaPandaFile(Tools.luapandaPathInUserProj)
+                                }
+                            });
+                        }
+                        else{
+                            // 本次插件运行期间不再提示
+                            UpdateManager.setCheckUpdate(false);
+                        };
+
+                    });
                 }
             }else{
                 //版本号异常，不做处理
@@ -41,9 +62,11 @@ export class UpdateManager{
         let luapandaContent = fs.readFileSync(Tools.getLuaPathInExtension());
         fs.writeFile(pandaPath, luapandaContent , function(err: NodeJS.ErrnoException){
             if(err){
-                DebugLogger.showTips("升级失败，", 1);
+                DebugLogger.showTips("升级失败, " + Tools.luapandaPathInUserProj + "写入失败! 可以手动替换此文件到github最新版", 1);
+            }else{
+                DebugLogger.showTips("升级成功, " + Tools.luapandaPathInUserProj + "版本已升级为 " + Tools.adapterVersion , 0);
             }
-            DebugLogger.showTips("升级成功", 0);
+            UpdateManager.setCheckUpdate(false);
         });
     }
 }
