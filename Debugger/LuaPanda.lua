53ae147d15c90f84405850897cc924ffae7f119b
bruce <hanyongtao@live.com>
2022-04-18 10:50:51 +0800

    disable lua diagnostic

--- Debugger/LuaPanda.lua
+++ Debugger/LuaPanda.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable
+
 -- Tencent is pleased to support the open source community by making LuaPanda available.
 -- Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
 -- Licensed under the BSD 3-Clause License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
